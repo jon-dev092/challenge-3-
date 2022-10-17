ad33763231d9a2fbec0e614246f4669974abb098
jon-dev092 <jp106833@yahoo.com>
2022-10-17 01:24:21 -0400

    Starter code push

---
+++ Assets/script.js
@@ -0,0 +1,155 @@
+// Assignment Code
+
+// global variables
+var num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+var upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var spec = ["!", "@", "#", "$", "%", "&", "*", "+"]
+var randomChar = [];
+
+
+
+var generatePassword = function () {
+
+  
+  var charLength = prompt('Please enter length of password; enter a number between "8" and "128".');
+ 
+
+  if (charLength < 8 || charLength > 128) {
+    alert('Must be a number between "8" and "128"');
+    return generatePassword();
+  } else if (!charLength) {
+    alert('A number value is needed before proceeding')
+    return generatePassword();
+  }
+
+
+
+  var charLower = confirm('Would you like to use lowercase characters?')
+
+  if (!charLower) {
+    charLower = '';
+  } else {
+    charLower = lower;
+  }
+
+  console.log(charLower);
+
+  
+
+
+
+  var charUpper = confirm('Would you like to use uppercase characters?')
+
+  if (!charUpper) {
+    charUpper = '';
+  } else {
+    charUpper = upper;
+  }
+
+  console.log(charUpper)
+
+
+
+  var charNum = confirm('Would you like to use number characters?')
+
+  if (!charNum) {
+    charNum = '';
+  } else {
+    charNum = num
+  }
+
+  console.log(charNum)
+
+
+
+  var charSpec = confirm('Would you like to use special characters?')
+
+  if (!charSpec) {
+    charSpec = '';
+  } else {
+    charSpec = spec;
+  }
+
+  console.log(charSpec)
+
+
+
+  var totalChar = charLower + charUpper + charNum + charSpec;
+
+  if (totalChar.length <= 0) {
+    alert('Atleast one type must be chosen');
+    return generatePassword();
+  }
+
+  console.log(totalChar)
+  console.log(totalChar.length)
+
+
+
+  if (charLower) {
+    randomChar = randomChar.concat(lower);
+  } else if (!charLower) {
+    charLower = '';
+  }
+
+  console.log(charLower)
+  console.log(randomChar)
+
+
+  if (charUpper) {
+    randomChar = randomChar.concat(upper);
+  } else if (!charUpper) {
+    charUpper = '';
+  }
+
+  console.log(charUpper)
+  console.log(randomChar)
+
+  if (charNum) {
+    randomChar = randomChar.concat(num);
+  } else {
+    num = '';
+  }
+
+  console.log(randomChar)
+
+
+  if (charSpec) {
+    randomChar = randomChar.concat(spec);
+  } else {
+    spec = '';
+  }
+
+  console.log(randomChar)
+
+
+
+
+  var finalPassword = []
+  for (i = 0; i < charLength; i++) {
+    var range =[Math.floor(Math.random() * randomChar.length)];
+    finalPassword = finalPassword + randomChar[range];
+  }
+
+  console.log(finalPassword)
+
+  return finalPassword;
+
+
+}
+
+
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
